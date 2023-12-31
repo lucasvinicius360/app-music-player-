d06e310e05175bf4c6cfdd1dd204c0a96b6d62fe
Lucas Vinicius <volgfan@gmail.com>
2023-12-31 10:52:54 -0300

    reenderizando a musica da api node

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:music_player/app/models/music_model.dart';
 import 'home_store.dart';
-
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -15,6 +15,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends ModularState<HomePage, HomeStore> {
   @override
+  void initState() {
+    super.initState();
+    store.findAll();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -25,17 +31,27 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
 
       backgroundColor: Colors.black,
 
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (_, int index) {
-          return ListTile(
-            onTap: () => Modular.to.pushNamed('/Player', arguments: 'assets/images/Linkinpark.jpg'),
-            leading: Image.asset('assets/images/Linkinpark.jpg'),
-            title: Text('Live in texas'),
-            subtitle: Text('Linkin Park'),
-            contentPadding: EdgeInsets.all(15),
-          );
+      body: FutureBuilder<List<MusicaModel>>(
+        builder: (context, constructor) {
+          future: store.musicList;
+          switch (constructor.data) {
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+              break;
+            case ConnectionState.done:
+              if (constructor.hasData) {
+                return buildList(constructor.data);
+              } else {
+                return Container();
+              }
+              break;
+            default:
+              return Container();
+          }
         },
+        future: null,
       ),
 
       // floatingActionButton: FloatingActionButton(
@@ -44,6 +60,22 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
       //   },
       //   child: Icon(Icons.add),
       // ),
+    );
+  }
+
+  ListView buildList(List<MusicaModel>? data) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, int index) {
+        return ListTile(
+          onTap: () => Modular.to
+              .pushNamed('/Player', arguments: 'assets/images/Linkinpark.jpg'),
+          leading: Image.asset('assets/images/Linkinpark.jpg'),
+          title: Text('Live in texas'),
+          subtitle: Text('Linkin Park'),
+          contentPadding: EdgeInsets.all(15),
+        );
+      },
     );
   }
 }
--- lib/app/modules/home/home_store.dart
+++ lib/app/modules/home/home_store.dart
@@ -1,14 +1,67 @@
 import 'package:mobx/mobx.dart';
+import 'package:music_player/app/models/music_model.dart';
+import 'package:music_player/app/repositories/music_repository.dart';
+
+// Importe a classe MusicRepository
 
 part 'home_store.g.dart';
 
 class HomeStore = HomeStoreBase with _$HomeStore;
 
 abstract class HomeStoreBase with Store {
+  // Crie uma instância de MusicRepository
+  final MusicRepository musicRepository = MusicRepository();
+
   @observable
   int counter = 0;
 
+  @observable
+  ObservableList<MusicaModel> musicList = ObservableList<MusicaModel>();
+
+  // @observable
+  // late ObservableFuture<List<MusicaModel>> test;
+
+
+
+  @action
   Future<void> increment() async {
     counter = counter + 1;
   }
+
+  @action
+  Future <void> findAll() async {
+    List<MusicaModel> musics = await musicRepository.findMusics();
+  }
+
+  // Adicione um método para buscar músicas
+  //   @action
+  //   Future<void> fetchMusics() async {
+  //     try {
+  //       // Chame o método findMusics() do MusicRepository
+  //       List<MusicaModel> musics = await musicRepository.findMusics();
+
+  //       // Atualize a lista de músicas
+  //       musicList.clear();
+  //       musicList.addAll(musics);
+  //     } catch (e) {
+  //       // Trate erros, se necessário
+  //       print("Erro ao buscar músicas: $e");
+  //     }
+  //   }
+
+  //   // Adicione um método para buscar músicas por banda
+  //   @action
+  //   Future<void> fetchMusicsByBand(String bands) async {
+  //     try {
+  //       // Chame o método findBands() do MusicRepository
+  //       List<MusicaModel> musics = await musicRepository.findBands(bands);
+
+  //       // Atualize a lista de músicas
+  //       musicList.clear();
+  //       musicList.addAll(musics);
+  //     } catch (e) {
+  //       // Trate erros, se necessário
+  //       print("Erro ao buscar músicas por banda: $e");
+  //     }
+  //   }
 }
