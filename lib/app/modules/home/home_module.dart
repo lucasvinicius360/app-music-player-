490abf06a8e737f9baff50caf5f5453a628140a4
lucasvinicius360 <volgfan@gmail.com>
2024-01-16 20:36:13 -0300

    page welcome

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:music_player/app/modules/home/home_welcome.dart';
 import 'package:music_player/app/modules/player/player_module.dart';
 import 'package:music_player/app/modules/player/player_page.dart';
 
@@ -14,7 +15,7 @@ class HomeModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => HomePage()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => HomeWelcome()),
     
     ChildRoute('/Player', child: (_, args) => PlayerPage(url: args.data)),
     // Remova uma das rotas '/Player' para evitar duplicatas
---
+++ lib/app/modules/home/home_welcome.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeWelcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
+        // ignore: prefer_const_constructors
+        decoration: BoxDecoration(
+            color: Colors.black,
+            // ignore: prefer_const_constructors
+            image: DecorationImage(
+                image: AssetImage("assets/images/img2.jpg"),
+                fit: BoxFit.cover,
+                opacity: 1.6)),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Music Player',
+                style: GoogleFonts.pacifico(fontSize: 50),
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Listen To Music Now',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('Dev de Jesus');
+                    },
+                    child: Container(
+                      padding:EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 123, 7),
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Text('Get Started'),
+                    ),
+                  )
+                ],
+              )
+            ]),
+      ),
+    );
+  }
+}
